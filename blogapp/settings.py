b8a263a1b9e5a70d889202f3a12cfb9e858aa6e3
unknown <yungwalexzy12@gmail.com>
2022-05-30 15:20:55 +0100

    set static file

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompresseedMinifestStaticFileStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
