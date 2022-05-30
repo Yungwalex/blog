d1752af61df9f55aa8e7b050b748d43eeb71348b
unknown <yungwalexzy12@gmail.com>
2022-05-30 17:25:07 +0100

    fear

--- myblog/urls.py
+++ myblog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 
-app_name = 'blogapp'
+app_name = 'myblog'
 
 
 
@@ -14,5 +14,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/',views.post_share, name='post_share'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
-    
+       
 ]
