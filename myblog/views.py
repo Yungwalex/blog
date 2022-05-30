29f63488432ef32440f5fee0d97016cee326240d
unknown <yungwalexzy12@gmail.com>
2022-05-30 16:02:56 +0100

    git

--- myblog/views.py
+++ myblog/views.py
@@ -16,18 +16,18 @@ def post_list(request, tag_slug=None):
     object_list = Post.published.all()
     tag=None
     if tag_slug:
-     tag =get_object_or_404(Tag, slug = tag_slug)
-     object_list = object_list.filter(tags__in=[tag])
-     paginator = Paginator(object_list, 3)
-     page = request.GET.get('page')
-     try:
+        tag =get_object_or_404(Tag, slug = tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
          posts = paginator.page(page)
-     except PageNotAnInteger:
+    except PageNotAnInteger:
          posts= paginator.page(1)
-     except EmptyPage:
+    except EmptyPage:
          posts= paginator.page(paginator.num_pages) 
          
-     return render(request, 'blog/blog/list.html', {'page': page, 'posts': posts, 'tags': tags})
+    return render(request, 'blog/blog/list.html', {'page': page, 'posts': posts, 'tags': tags})
  
 def post_detail(request, year, month, day, post):
     post =get_object_or_404(post, slug=post, statyus= 'published', Publish__year= year, publish__month= month, publish__day= day)
