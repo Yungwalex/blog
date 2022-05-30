24ad600b47b3aeb4a073c04f039973ce9083cb60
unknown <yungwalexzy12@gmail.com>
2022-05-30 17:16:39 +0100

    ser

--- myblog/urls.py
+++ myblog/urls.py
@@ -3,25 +3,16 @@ from . import views
 
 
 
-app_name = 'myblog'
+app_name = 'blogapp'
 
 
 
 urlpatterns = [
-
     #post views
-
     path('', views.post_list, name='post_list'),
-
     #path('', views.PostListView.as_view(), name= 'post_list'),
-
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, 
-
-    name='post_detail'),
-
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/',views.post_share, name='post_share'),
-
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
-       
-
+    
 ]
