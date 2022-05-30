3642ce9390d3073c6332f375b55641aad25f3dbb
unknown <yungwalexzy12@gmail.com>
2022-05-30 16:22:44 +0100

    set

--- myblog/views.py
+++ myblog/views.py
@@ -27,7 +27,7 @@ def post_list(request, tag_slug=None):
     except EmptyPage:
          posts= paginator.page(paginator.num_pages) 
          
-    return render(request, 'blog/blog/list.html', {'page': page, 'posts': posts, 'tag': tag})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
  
 def post_detail(request, year, month, day, post):
     post =get_object_or_404(post, slug=post, statyus= 'published', Publish__year= year, publish__month= month, publish__day= day)
